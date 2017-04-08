2639a16f54d01781f533eef3e6ae0147bd9c4b14
TangBoHao <tangbohao0215@163.com>
2017-04-08 15:50:22 +0800

    first

--- function.php
+++ function.php
@@ -3,7 +3,7 @@
 //模型调用函数
 function M($name)
 {
-	require_once('lib/Model'.$name.'Model.class.php');
+	require_once('libs/Model/'.$name.'Model.class.php');
 	eval('$obj=new '.$name.'Model()');
 	return $obj;
 }
@@ -11,7 +11,7 @@ function M($name)
  //视图调用函数
 function V($name)
 {
-	require_once('lib/View'.$name.'View.class.php');
+	require_once('libs/View/'.$name.'View.class.php');
 	eval('$obj=new '.$name.'View()');
 	return $obj;
 }
@@ -19,11 +19,13 @@ function V($name)
 //方法调用函数
 function C($name,$method)
 {
-	require_once('lib/Controller/'.$name.'Controller.class.php');
-	eval('$obj=new'.$name.'Controller();$obj->'.$method.'();');
+	require_once('libs/Model/'.$name.'Model.class.php');
+	require_once('libs/View/'.$name.'View.class.php');
+	require_once('libs/Controller/'.$name.'Controller.class.php');
+	eval('$obj=new '.$name.'Controller();$obj->'.$method.'();');
 }
 
-function daddslashes(&str)
+function daddslashes($str)
 {
 	//首先判断魔法符号是否打开
 	return (!get_magic_quotes_gpc())?addslashes($str):$str;
--- index.php
+++ index.php
@@ -2,7 +2,10 @@
 //url形式： index.php?controller=控制器名&method=方法名
 require_once('function.php');
 $controllerAllow=array('test','index');
-$methodAllow=array('test','index');
-$controller=daddslashes($_GET['controller']);
-$method=daddslashes($_GET['method']);
+$methodAllow=array('test','index','show');
+$controller=in_array($_GET['controller'], $controllerAllow)?daddslashes($_GET['controller']):"index";
+$method=in_array($_GET['method'], $methodAllow)?daddslashes($_GET['method']):"index";
+//$controller=daddslashes($_GET['controller']);
+//$method=daddslashes($_GET['method']);
+
 C($controller,$method);
